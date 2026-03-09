978b10a7232bc8204ea3efa5890d712c104751bf
Ќазими Фејми <fejmi.kjazimi@students.finki.ukim.mk>
2026-03-10 00:59:26 +0100

    .

--- Django/supplements-project/supplementStoreApp/admin.py
+++ Django/supplements-project/supplementStoreApp/admin.py
@@ -3,6 +3,14 @@ from .models import Manufacturer,Category, Supplement
 
 # Register your models here.
 
-admin.site.register(Manufacturer)
-admin.site.register(Category)
-admin.site.register(Supplement)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "isActive")
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("name", "date", "type")
+
+@admin.register(Supplement)
+class SupplementAdmin(admin.ModelAdmin):
+    list_display = ("name","description","category", "user", "photo", "price", "quantity")
